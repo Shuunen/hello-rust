ff9c23ae237da14c2394d5d2969eb536722f95e0
jordan.boyer <boyer.jordan42@gmail.com>
2023-06-20 21:57:47 +0200

    better string example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
 
-fn hello(name: String) {
-    println!("Hello, {}", name);
+fn hello(name: &str) {
+    println!("Hello, {}", name); // {} is a placeholder for the value of name. you can chain multiple {} to print multiple values
 }
 
 fn main() {
-    let arg1: Vec<String> = env::args().collect();
-    hello(arg1[1].clone());
+    let mut args = env::args(); // mut is needed here because args() returns an iterator and calling nth() on an iterator consumes it
+    let arg1 = args.nth(1).unwrap(); // need to unwrap because nth() returns an Option and we don't want to deal with none values for now
+    hello(&arg1); // arg1 is of type String and needs to be borrowed to be passed to hello() because hello() expects a &str type
+    hello("jojo") // sting literals are of type &str and can be passed to hello() without borrowing
 }
