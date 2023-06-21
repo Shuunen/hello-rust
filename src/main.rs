f2a865317240a426c562508254bfbe7d0f573231
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-06-21 15:21:57 +0200

    show data & time & write to FS

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use std::env;
 
 fn hello(name: &str) {
-    println!("Hello, {}", name); // {} is a placeholder for the value of name. you can chain multiple {} to print multiple values
+    let mut output = format!("\nHello, {} :)\n", name);
+    // add the date and time to the output
+    output.push_str(&format!(
+        "\nThe date and time is: {}\n",
+        chrono::offset::Local::now()
+    ));
+    // print the output to the terminal
+    println!("{}", &output);
+    // write the output to a file
+    std::fs::write("output.txt", output);
+    println!("output.txt created or overwritten");
 }
 
 fn main() {
-    let mut args = env::args(); // mut is needed here because args() returns an iterator and calling nth() on an iterator consumes it
-    let arg1 = args.nth(1).unwrap(); // need to unwrap because nth() returns an Option and we don't want to deal with none values for now
-    hello(&arg1); // arg1 is of type String and needs to be borrowed to be passed to hello() because hello() expects a &str type
-    hello("jojo") // sting literals are of type &str and can be passed to hello() without borrowing
+    if let Some(name) = env::args().nth(1) {
+        hello(&name);
+    } else {
+        println!("Please provide an argument");
+    }
 }
